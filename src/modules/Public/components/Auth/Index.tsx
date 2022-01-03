116e8851c3fc0148390af790b6d0fcbaf6f31ad1
Roshan Dev-Business Partner <Roshan.Dev-External@alfuttaim.com>
2022-01-03 16:56:43 +0400

    added icons for the auth module

--- src/modules/Public/components/Auth/Index.tsx
+++ src/modules/Public/components/Auth/Index.tsx
@@ -1,21 +1,27 @@
-import { IonLabel, IonSegment, IonSegmentButton } from "@ionic/react";
+import { IonIcon, IonLabel, IonSegment, IonSegmentButton } from "@ionic/react";
+import { personAddOutline, personOutline } from "ionicons/icons";
 import { useState } from "react";
 import Login from "./Login";
 import Register from "./Register";
 
 const Auth: React.FC = () => {
-  const [segment, setSegment] = useState<'login' | 'register'>('login');
+  const [segment, setSegment] = useState<"login" | "register">("login");
   return (
     <>
-      <IonSegment onIonChange={(e) => setSegment(e.detail.value as any)} value={segment}>
+      <IonSegment
+        onIonChange={(e) => setSegment(e.detail.value as any)}
+        value={segment}
+      >
         <IonSegmentButton value="login">
+          <IonIcon icon={personOutline} />
           <IonLabel>Login</IonLabel>
         </IonSegmentButton>
         <IonSegmentButton value="register">
+          <IonIcon icon={personAddOutline} />
           <IonLabel>Register</IonLabel>
         </IonSegmentButton>
       </IonSegment>
-      {segment === 'login' ? <Login /> : <Register />}
+      {segment === "login" ? <Login /> : <Register />}
     </>
   );
 };
